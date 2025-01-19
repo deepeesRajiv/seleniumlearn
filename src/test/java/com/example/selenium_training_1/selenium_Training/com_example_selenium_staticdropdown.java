3be1a85623d173175fd8312db208aaa0f9c14f8e
Rajiv Deshpande <Rajiv Deshpande@DESKTOP-S8P5USB>
2025-01-19 15:29:11 +0530

    dynnamic search

--- src/test/java/com/example/selenium_training_1/selenium_Training/com_example_selenium_staticdropdown.java
+++ src/test/java/com/example/selenium_training_1/selenium_Training/com_example_selenium_staticdropdown.java
@@ -15,27 +15,27 @@ public class com_example_selenium_staticdropdown {
 		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Rajiv Deshpande\\Documents\\selenium_Training\\selenium_Training\\driver\\chromedriver-win64\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		driver.get("https://www.icicibank.com/personal-banking/accounts/savings-account");
-		//driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		//driver.get("https://www.icicibank.com/personal-banking/accounts/savings-account");
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
 		//driver.get("https://rahulshettyacademy.com/angularpractice/");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
 		
-//		WebElement drop =  driver.findElement(By.id("dropdown-class-example"));
+		WebElement drop =  driver.findElement(By.id("dropdown-class-example"));
+		
+		Select selectfromdropdown = new Select(drop);
+		selectfromdropdown.selectByIndex(2);
+		
+//        driver.findElement(By.xpath("//li//div//a[@class = 'menu-nav-tablink']//span[text() ='Deposits']"));
+// 
+//        List<WebElement> menu =   driver.findElements(By.xpath("//ul[@class='mega-menu-items']//div[@class='menu-nav-tab']//span[@class = 'menu-txt']"));
 //		
-//		Select selectfromdropdown = new Select(drop);
-//		selectfromdropdown.selectByIndex(2);
-		
-        driver.findElement(By.xpath("//li//div//a[@class = 'menu-nav-tablink']//span[text() ='Deposits']"));
- 
-        List<WebElement> menu =   driver.findElements(By.xpath("//ul[@class='mega-menu-items']//div[@class='menu-nav-tab']//span[@class = 'menu-txt']"));
-		
-        WebElement topmenu;
-        
-        for(int i=0;i<menu.size();i++) {
-        	topmenu= menu.get(i);
-        	System.out.println(topmenu.getText());
-        }
+//        WebElement topmenu;
+//        
+//        for(int i=0;i<menu.size();i++) {
+//        	topmenu= menu.get(i);
+//        	System.out.println(topmenu.getText());
+//        }
 
  
 	
